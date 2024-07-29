6f700e8e42be7c3c70268c61c416e8911f1ef16b
Lian <jlianzk@gmail.com>
2024-07-29 04:41:42 +0000

    Final

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('@openzeppelin/hardhat-upgrades');
+
+// Remember to use the private key of a testingrequire("@nomicfoundation/hardhat-toolbox");
+// For better security practices, it's recommended to use npm i dotenv for storing secret variables
+// Use configuration-variables in hardhat to set PRIVATE_KEY variable
+
+const PRIVATE_KEY = vars.get("PRIVATE_KEY");
+
+module.exports = {
+  defaultNetwork: "swisstronik",
+  solidity: "0.8.19",
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/",
+      accounts: [`${PRIVATE_KEY}`],
+    },
+  },
+};
